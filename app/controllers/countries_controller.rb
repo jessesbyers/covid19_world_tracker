648e9e0ae94fd2ceb0d099f0fcfd483303a1f422
jessesbyers <jesse.s.byers@gmail.com>
2020-05-06 08:23:13 -0400

    added country controller and country validations

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,2 +1,34 @@
 class CountriesController < ApplicationController
+
+
+    def index
+        @countries = Country.all
+        render json: @countries.to_json(:except => [:updated_at, :created_at])
+    end
+
+    def show
+        @country = Country.find(params[:id])
+        render json: @country.to_json(:except => [:updated_at, :created_at])
+    end
+
+    def create
+        @country = Country.new(country_params)
+        if @country.save
+            render json: @country.to_json(:except => [:updated_at, :created_at])
+        else
+            render json: {error: "Country cannot be saved to the database. Please try again."}
+        end
+    end
+
+    def destroy
+        @country = Country.find(params[:id])
+        @country.destroy
+        render json: {alert: "Country Deleted"}
+    end
+
+    private
+
+    def country_params
+        params.require(:country).permit(:name, :slug, :data_url)
+    end
 end
--- app/models/country.rb
+++ app/models/country.rb
@@ -1,5 +1,7 @@
 class Country < ApplicationRecord
     has_many :charts
 
-    # add backend validations
+    validates :name, presence: true
+    validates :slug, presence: true
+    validates :data_url, presence: true
 end
